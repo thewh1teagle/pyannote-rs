6a96fbfce12a284b768fa44a98663e7be4a55dce
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-08-06 09:25:58 +0300

    knf rs from git

--- src/embedding.rs
+++ src/embedding.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 
 use eyre::Result;
-use ndarray::{Array1, Array2};
+use ndarray::Array2;
 use ort::Session;
 
 use crate::session;
@@ -17,30 +17,9 @@ impl EmbeddingExtractor {
     }
 
     pub fn compute(&mut self, samples: &[f32]) -> Result<Vec<f32>> {
-        // Compute fbank mel features
-        let sampling_rate = 16000.0;
-        let fft_size = 400;
-        let n_mels = 80;
-        let f_min = None;
-        let f_max = None;
-        let hkt = false;
-        let norm = true;
-        let features_f64 =
-            mel_spec::mel::mel(sampling_rate, fft_size, n_mels, f_min, f_max, hkt, norm);
-
-        let features: Array2<f32> = features_f64.mapv(|x| x as f32);
-        let mut features = features.t().to_owned(); // Transpose the array
-        let mean = features
-            .mean_axis(ndarray::Axis(0))
-            .expect("Failed to compute mean")
-            .insert_axis(ndarray::Axis(0)); // Add batch axis for broadcasting
-        features -= &mean;
-
-        let features_3d = features.insert_axis(ndarray::Axis(0)); // Add batch dimension
-
-        // # Apply CMN (Cepstral Mean Normalization)
-        // features = features - np.mean(features, axis=0)
-        let inputs = ort::inputs! ["feats" => features_3d.view()]?;
+        let features: Array2<f32> = knf_rs::compute_fbank(samples)?;
+        let features = features.insert_axis(ndarray::Axis(0)); // Add batch dimension
+        let inputs = ort::inputs! ["feats" => features.view()]?;
 
         let ort_outs = self.session.run(inputs)?;
         let ort_out = ort_outs
--- src/main.rs
+++ src/main.rs
@@ -19,25 +19,37 @@ fn main() -> Result<()> {
     // FYI: it doesn't work but that's the direction.
     let mut embedding_extractor =
         EmbeddingExtractor::new(Path::new("wespeaker_en_voxceleb_CAM++.onnx")).unwrap();
-    let mut embedding_manager = EmbeddingManager::new(100);
+    let mut embedding_manager = EmbeddingManager::new(5);
 
     let segments = segment::segment(&samples, sample_rate, model_path)?;
 
     for (start, end) in segments {
-        // Compute index
-        let start_idx = start as usize;
-        let end_idx = end as usize;
+        // Convert start and end times to sample indices
+        let start_idx = (start * (sample_rate as f64)) as usize;
+        let end_idx = (end * (sample_rate as f64)) as usize;
+
+        // Ensure indices are within bounds
         let start_idx = start_idx.min(samples_f32.len() - 1);
         let end_idx = end_idx.min(samples_f32.len());
+
+        // Extract segment samples
         let segment_samples = &samples_f32[start_idx..end_idx];
-        let embedding_result = embedding_extractor.compute(&segment_samples).unwrap();
-        let speaker = embedding_manager
-            .get_speaker(embedding_result, 0.5)
-            .unwrap_or(usize::MAX);
-        println!(
-            "Segment: start = {:.2}, end = {:.2}, speaker = {}",
-            start, end, speaker
-        );
+
+        // Compute embedding
+        match embedding_extractor.compute(&segment_samples) {
+            Ok(embedding_result) => {
+                let speaker = embedding_manager
+                    .get_speaker(embedding_result, 0.5)
+                    .unwrap_or(usize::MAX);
+                println!(
+                    "Segment: start = {:.2}, end = {:.2}, speaker = {}",
+                    start, end, speaker
+                );
+            }
+            Err(error) => {
+                println!("error: {:?}", error);
+            }
+        }
     }
 
     Ok(())
