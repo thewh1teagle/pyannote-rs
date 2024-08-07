107eedb02e31ccc0213c979f91c0fcefabd1f58f
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-08-07 18:54:12 +0300

    misc

--- examples/diarize.rs
+++ examples/diarize.rs
@@ -1,3 +1,10 @@
+/*
+wget https://github.com/pengzhendong/pyannote-onnx/raw/master/pyannote_onnx/segmentation-3.0.onnx
+wget https://github.com/k2-fsa/sherpa-onnx/releases/download/speaker-recongition-models/wespeaker_en_voxceleb_CAM++.onnx
+wget https://github.com/thewh1teagle/sherpa-rs/releases/download/v0.1.0/6_speakers.wav
+cargo run --example diarize
+*/
+
 use pyannote_rs::EmbeddingExtractor;
 use pyannote_rs::EmbeddingManager;
 
