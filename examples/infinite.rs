1e5f1ea91c239d2cebc0379bee426b89559a3178
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-08-09 23:40:39 +0300

    misc

--- examples/infinite.rs
+++ examples/infinite.rs
@@ -2,7 +2,7 @@
 wget https://github.com/pengzhendong/pyannote-onnx/raw/master/pyannote_onnx/segmentation-3.0.onnx
 wget https://github.com/k2-fsa/sherpa-onnx/releases/download/speaker-recongition-models/wespeaker_en_voxceleb_CAM++.onnx
 wget https://github.com/thewh1teagle/sherpa-rs/releases/download/v0.1.0/6_speakers.wav
-cargo run --example infinite
+cargo run --example infinite 6_speakers.wav
 */
 
 use pyannote_rs::EmbeddingExtractor;
