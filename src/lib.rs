fa73e36bb6e9778aaa240547a38975f44fc3d5b7
altunenes <enesaltun2@gmail.com>
2025-10-08 15:34:14 +0300

    mod plda

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,13 @@ mod session;
 
 mod embedding;
 mod identify;
+mod plda;
 mod segment;
 mod wav;
 
 pub use embedding::EmbeddingExtractor;
 pub use identify::EmbeddingManager;
 pub use knf_rs::{compute_fbank, convert_integer_to_float_audio};
+pub use plda::PLDA;
 pub use segment::{get_segments, Segment};
 pub use wav::read_wav;
