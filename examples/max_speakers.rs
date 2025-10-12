f49312fa41ebc51c10a10de9e772d84fe36193ee
rdcm <rdcm@protonmail.ch>
2025-10-13 01:30:37 +0300

    samples improvement

--- examples/max_speakers.rs
+++ examples/max_speakers.rs
@@ -1,43 +1,40 @@
+use eyre::Result;
 use pyannote_rs::{EmbeddingExtractor, EmbeddingManager};
 
-fn main() {
+fn main() -> Result<()> {
     let audio_path = std::env::args().nth(1).expect("Please specify audio file");
-    let (samples, sample_rate) = pyannote_rs::read_wav(&audio_path).unwrap();
+    let (samples, sample_rate) = pyannote_rs::read_wav(&audio_path)?;
     let max_speakers = 6;
 
-    let mut extractor = EmbeddingExtractor::new("wespeaker_en_voxceleb_CAM++.onnx").unwrap();
+    let mut extractor = EmbeddingExtractor::new("wespeaker_en_voxceleb_CAM++.onnx")?;
     let mut manager = EmbeddingManager::new(6);
 
-    let segments =
-        pyannote_rs::get_segments(&samples, sample_rate, "segmentation-3.0.onnx").unwrap();
+    let segments = pyannote_rs::get_segments(&samples, sample_rate, "segmentation-3.0.onnx")?;
 
     for segment in segments {
         match segment {
             Ok(segment) => {
-                if let Ok(embedding) = extractor.compute(&segment.samples) {
-                    let speaker = if manager.get_all_speakers().len() == max_speakers {
-                        manager
-                            .get_best_speaker_match(embedding.collect())
-                            .map(|s| s.to_string())
-                            .unwrap_or("?".into())
+                let speaker = extractor.compute(&segment.samples).ok().map(|embedding| {
+                    let result = if manager.get_all_speakers().len() == max_speakers as usize {
+                        manager.get_best_speaker_match(embedding.collect())
                     } else {
                         manager
                             .search_speaker(embedding.collect(), 0.5)
-                            .map(|s| s.to_string())
-                            .unwrap_or("?".into())
+                            .ok_or(eyre::eyre!("No matching speaker found with threshold 0.5"))
                     };
-                    println!(
-                        "start = {:.2}, end = {:.2}, speaker = {}",
-                        segment.start, segment.end, speaker
-                    );
-                } else {
-                    println!(
-                        "start = {:.2}, end = {:.2}, speaker = ?",
-                        segment.start, segment.end
-                    );
-                }
+                    result.map(|s| s.to_string()).unwrap_or("?".into())
+                });
+
+                println!(
+                    "start = {:.2}, end = {:.2}, speaker = {}",
+                    segment.start,
+                    segment.end,
+                    speaker.unwrap_or("???".into())
+                );
             }
             Err(error) => eprintln!("Failed to process segment: {:?}", error),
         }
     }
+
+    Ok(())
 }
